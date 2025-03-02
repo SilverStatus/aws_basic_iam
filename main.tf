29ad8d322ba94b993b8d02bb1ca6bb487c27bf59
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-03-02 14:24:48 +0700

    attach iam role to user

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
+#define the provider
 provider "aws" {
   region = var.region
 }
 
+#create new user iam on aws
 resource "aws_iam_user" "admin-user"{
     name = var.iam_user
     tags = {
@@ -9,6 +11,39 @@ resource "aws_iam_user" "admin-user"{
     }
 }
 
+#create new policy for user
+resource "aws_iam_policy" "adminUser" {
+    name = "adminUser"
+    description = "Allow full access to all resources"
+    policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": "*",
+                "Resource": "*"
+            }
+        ]
+    }
+    EOF
+}
+
+resource "aws_iam_policy_attachment" "adminUser" {
+    name = "adminUser"
+    users = aws_iam_user.admin-user.name
+    policy_arn = aws_iam_policy.adminUser.arn
+  
+}
+
 output "iam_user" {
     value = aws_iam_user.admin-user.name
+}
+
+output "iam_policy" {
+    value = aws_iam_policy.adminUser.arn
+}
+
+output "iam_policy_attachment" {
+    value = aws_iam_policy_attachment.adminUser.name
 }
